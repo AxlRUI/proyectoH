306bdf3779d1b2f0af1296fb65fe62adbf9c16de
Deadpool789 <tsunadecimo123@gmail.com>
2019-12-07 10:26:08 -0600

    Update recuperar.php

--- phpDocs/recuperar.php
+++ phpDocs/recuperar.php
@@ -11,7 +11,8 @@
         $QueryBuscar = "SELECT * FROM Pacientes WHERE curp = $CURP AND Pass = $Pass;";
         if($resultado = $db -> query($QueryBuscar)){
             $rowPaciente = $resultado ->fetch_array(MYSQLI_NUM);
-            $stringPacienteJson ;
+            $stringJsonPaciente = $rowPaciente[0] + $rowPaciente[1] +$rowPaciente[2]+$rowPaciente[3]+$rowPaciente[4]+$rowPaciente[5]+$rowPaciente[6]+$rowPaciente[7]+$rowPaciente[8]+$rowPaciente[9];
+            echo json_encode($stringJsonPaciente);
             $QueryCartilla = "SELECT * FROM Cartilla WHERE CveUnicaPaciente = $rowPaciente[0];";
             if($resultadoV = $db ->query($QueryCartilla) ){
                 while($rowCartilla = $resultadoV -> fetch_array(MYSQLI_NUM) ){                 
