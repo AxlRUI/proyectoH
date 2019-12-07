546922ce6648685a54a05e8c6d3a4f66b8f6398f
Deadpool789 <tsunadecimo123@gmail.com>
2019-12-07 11:25:47 -0600

    Update login.php

--- phpDocs/login.php
+++ phpDocs/login.php
@@ -8,15 +8,16 @@ if($db ->connect_errno){
     exit();
 }else{
     $CURP = $_POST['CURP'];
-    $Pass = $_POST['Pass'];
-    $QueryBuscar = "SELECT * FROM Pacientes WHERE curp = $CURP AND Pass = $Pass;";
+    $Pass = $_POST['PASS'];
+    $QueryBuscar = "SELECT * FROM Pacientes WHERE curp = '$CURP' AND Pass = '$Pass';";
     if($resultado = $db -> query($QueryBuscar)){
         $rowPaciente = $resultado -> fetch_array(MYSQLI_NUM);
         $_SESSION['Usuario'] = $rowPacinte[9];
         $_SESSION['Pass'] = $rowPaciente[7];
         header("Location: ../app.php");
-
-
+    }else{
+        $string = "Error";
+        echo json_encode($string);
     }
 }
 ?>
