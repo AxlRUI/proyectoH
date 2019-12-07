83443385810da2baedfb23950f9fba6992c6b6ff
Deadpool789 <tsunadecimo123@gmail.com>
2019-12-06 19:36:51 -0600

    Create conect.php

---
+++ phpDocs/conect.php
@@ -0,0 +1,11 @@
+<?php
+    function conectarDB (){
+        $database = "u582402068_hackathon"; 
+        $username = "u582402068_osiris";
+        $password = "123qwe";
+        $host = "sql52.main-hosting.eu";
+        $db mysqli_connect();
+        return $db;
+    }
+
+?>
